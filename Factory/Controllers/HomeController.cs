9aaf1b640064a3f2a6c7e6ae6a30f416e5c731f3
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-18 16:09:19 -0700

    Add Engineers/Machines to splash page

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly FactoryContext _db;
+
+      public HomeController(FactoryContext db)
+      {
+        _db = db;
+      }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
+        ViewData["engineers"] = _db.Engineers.ToList();
+        ViewData["machines"] = _db. Machines.ToList();
         return View();
       }
 
