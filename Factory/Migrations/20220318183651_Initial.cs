d63bbad9ae269aa080866d42e807e0e06890d164
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-18 13:53:05 -0700

    Add README file and some styling

--- Factory/Migrations/20220318183651_Initial.cs
+++ Factory/Migrations/20220318183651_Initial.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace SillystringFactory.Migrations
+namespace Factory.Migrations
 {
     public partial class Initial : Migration
     {
