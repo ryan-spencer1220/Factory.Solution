a033a10ee5a1562d9fcee02c664547c002bf8d9e
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-03-18 11:35:21 -0700

    Change naming conventions to meet prompt guidelines

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+    {
+        public Engineer()
+        {
+            this.JoinEntities = new HashSet<EngineerMachine>();
+        }
+
+        public int EngineerId { get; set; }
+        public string Name { get; set; }
+        public string EnginnerStatus { get; set; }
+        public virtual ICollection<EngineerMachine> JoinEntities { get; set; }
+    }
+}
